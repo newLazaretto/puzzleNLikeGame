d7d2d8ae483834f18fbc3343533a8e8d20b963de
newLazaretto <97002110+newLazaretto@users.noreply.github.com>
2022-08-01 13:57:56 -0300

    Adição dos atributos das classes
    Adição dos dados, métodos e lógica suficiente para rodar um protótipo do jogo. Ainda sem comandos de usuário

--- Jogador.java
+++ Jogador.java
@@ -2,4 +2,15 @@ package Game;
 
 public class Jogador {
 
+	
+	private int pontos = 0;
+	private String nome;
+	
+	public Jogador(String nome) {
+		this.nome = nome;
+	}
+	
+	public void increasePontos(int pontos) {
+		this.pontos = pontos;
+	}
 }
--- Main.java
+++ Main.java
@@ -1,14 +1,33 @@
 package Game;
+import java.io.IOException;
+import Board.Tabuleiro;
 
 public class Main {
 
+public static void jogar(Tabuleiro tabuleiro, Jogador jogador){
 
-public static void main(String[] args) {
-	
-	
-	
+	while(true) {
+
+		tabuleiro.setPedrinhas();
+		tabuleiro.criaTabuleiro();
+		while(true) {
+			if (tabuleiro.checarAcerto() == true) {
+				break;
+			}
+			tabuleiro.printTabuleiro();
+		}
+	}
 	
 }
+public static void main(String[] args){
+	
+	
+	Tabuleiro tabuleiro = new Tabuleiro(3);
+	Jogador jogador = new Jogador("Lazaro");
+	
+	jogar(tabuleiro, jogador);
+	
+	}
 
 
 }
